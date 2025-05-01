7c55af2c5ee00a246c7abf15cfd6bc37c2a46224
neyshankhondoker <neyshankhondoker@gmail.com>
2025-05-01 21:12:27 +0000

    lec01_variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,6 +1,6 @@
 const accountID = 102359;   // cannot be changed
 let accountEmail = "aryan@gmail.com";   // can be changed
-var accountPassword = "012$22@PrT";     // doesn't follow block scope and functional scope 
+var accountPassword = "012$22@GrT";     // doesn't follow block scope and functional scope 
 accountCity = "Singapore City";     // never ever
 let accountState;   // undefined
 
