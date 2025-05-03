801acd22bffd620c1c0800f5c21515685f3f6bc5
neyshankhondoker <neyshankhondoker@gmail.com>
2025-05-03 18:13:01 +0000

    lec07_strings

---
+++ 01_basics/07_strings.js
@@ -0,0 +1,14 @@
+const username = "Aryan";
+const email = "aryan@gmail.com";
+
+const message = `Hello ${username}, your email is ${email}.`;
+console.log(message);
+
+const gameName = new String("ryan-007");    // access to many string methods (built-in)
+
+console.log(gameName.charAt(3));
+console.log(gameName.indexOf("a"));
+console.log(gameName.split("-"));
+console.log(gameName.replace("-", "."));
+
+// .... .... .... many more
