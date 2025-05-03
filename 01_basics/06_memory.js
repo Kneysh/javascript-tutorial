cc4611cb305ef7fcb5d63d00167ad522eb1a9771
neyshankhondoker <neyshankhondoker@gmail.com>
2025-05-03 17:43:19 +0000

    lec06_memory

---
+++ 01_basics/06_memory.js
@@ -0,0 +1,7 @@
+// JS uses two types of memory: Stack (Premitive) and Heap (Reference)
+
+// Premitive data creates a copy of the original when assigned to a new variable.
+// Reference data creates a reference to the original when assigned to a new variable.
+
+// Any changes in Stack Data or Premitive data doesn't affect the original data.
+// Any changes in Heap Data or Reference data also applies to the main data as it is the reference point.
