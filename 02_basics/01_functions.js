115fbc69019a8fdf6020e90828aeecd68ec319bd
neyshankhondoker <neyshankhondoker@gmail.com>
2025-05-05 08:37:56 +0000

    lec12_functions-basics

--- 02_basics/01_functions.js
+++ 02_basics/01_functions.js
@@ -4,8 +4,29 @@ function sayMyName(){
 sayMyName();
 
 function welcomeMessage(username = "Player"){
-    return `Hello ${username = "User"}, welcome to the game!`   // "User" has higher specificity than "Player"
+    return `Hello ${username = "User"}, welcome to the game!`;   // "User" has higher specificity than "Player"
 }
 console.log(welcomeMessage());
 
 // A function stops at return. Nothing gets executed after that.
+
+
+// A function can take infinite arguments using "...parameter".
+function cartPrices(...prices){
+    return prices;  // returns an array of all the arguments passed
+}
+console.log(cartPrices(200, 250, 1000));
+
+
+// A function can take any datatype as an argument.
+
+function priceMessage(product){
+    return `${product.name} is priced at ${product.price}.`;
+}
+
+const addedProduct = {
+    name: "RTX 5060",
+    price: "2000USD"
+}
+
+console.log(priceMessage(addedProduct));
